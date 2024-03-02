8f3e3ef30a8cbd2a6aff08affb9824309310fd43
SoulThalion <adrielguijarro@gmail.com>
2024-03-02 19:14:46 +0000

    comentario borrado

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,22 +3,6 @@ const { sequelize } = require('../../database')
 
 const User = sequelize.define(
 	'user',
-	
-	/*{userName: sequelize.STRING}, {indexes:[{allowNull:false, fields: ['userName']}]},
-
-	{nickName: sequelize.STRING(20)}, {indexes:[{unique:true, allowNull:false, fields: ['nickName']}]},
-
-	{avatar: sequelize.ENUM('wizard', 'paladin', 'warrior', 'rogue', 'cleric', 'druid', 'bard', 'hunter')}, {indexes:[{allowNull:false, default: 'wizard', fields: ['nickName']}]},
-	
-	{email: sequelize.STRING(40), validate: {isEmail: true}}, {indexes:[{unique:true, allowNull:false, fields: ['email']}]},
-
-	{role: sequelize.ENUM('user', 'admin')}, {indexes:[{allowNull:false, default: 'user', fields: ['role']}]},
-
-	{password: sequelize.STRING()}, {indexes:[{allowNull:false, fields: ['password']}]},
-
-	{level: sequelize.INTEGER()}, {indexes:[{allowNull:false, default: 0, fields: ['level']}]},
-	
-	)*/
 	{
 		userName: {
 			type: DataTypes.STRING(20),
